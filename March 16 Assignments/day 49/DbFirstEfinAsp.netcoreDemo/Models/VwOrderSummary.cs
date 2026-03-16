8ed70bfcc4322fcddc7e9908d026e4429fded6c9
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2026-03-16 13:52:33 +0530

    day 49

---
+++ March 16 Assignments/day 49/DbFirstEfinAsp.netcoreDemo/Models/VwOrderSummary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstEfinAsp.netcoreDemo.Models;
+
+public partial class VwOrderSummary
+{
+    public int OrderId { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public decimal? TotalAmount { get; set; }
+
+    public long? TotalRows { get; set; }
+}
