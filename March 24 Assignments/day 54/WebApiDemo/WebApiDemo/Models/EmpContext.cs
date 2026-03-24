27ddc988993a675803ecae45c0549d61491840e2
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2026-03-24 15:03:27 +0530

    March 24 Assignments

---
+++ March 24 Assignments/day 54/WebApiDemo/WebApiDemo/Models/EmpContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDemo.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+
+    }
+}
